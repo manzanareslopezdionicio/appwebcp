c68acb831a0323dfaa8b88b82d73e41c4f40c49b
manzanareslopezdionicio <manzanareslopezdionicio@gmail.com>
2022-11-12 20:07:43 -0600

    actualizar plantillas

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template('/registros/index.html')
+    return render_template('index.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
