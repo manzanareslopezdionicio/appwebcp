cfecbf3de737f7b9e97a4d6b25ac922531a7408e
manzanareslopezdionicio <118032880+manzanareslopezdionicio@users.noreply.github.com>
2022-12-09 14:49:46 -0600

    validar formulario

--- templates/static/jquery_mask.js
+++ templates/static/jquery_mask.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-    $("#cedula").mask('DDD-DDDDDD-DDDDA', {'translation':{
-        D: {pattern: /[0-9]/},
-        A: {pattern: /[A-Za-z]/},
+    $('#cedula').mask('SSS-SSSSSS-SSSSA', {'translation': {
+        'S': {pattern: /[0-9]/},
+        'A': {pattern: /[A-Za-z]/}
     }});
 
-    $("#telefono").mask('0000-0000');
+    $('#telefono').mask('(000) 0000-0000');
 });
